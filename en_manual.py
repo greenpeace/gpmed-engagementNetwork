de41b06326a9fd2b6b31f2c4310f92bfaa1b3801
Fabian Krautgasser <fkrautgasser@posteo.org>
2019-01-17 18:49:19 +0100

    command line arguments, logger setup, external config file

--- en_manual.py
+++ en_manual.py
@@ -3,6 +3,7 @@ database."""
 
 import bs4
 import lxml
+import json
 import logging
 
 import datetime as dt
@@ -10,8 +11,57 @@ import requests as req
 import mysql.connector as con
 
 # # TODO:
-# 1) set up logger + output
-# 2) command line arguments for custom time interval
-# 3) external file(s) for credentials
-# 4) error handling for requested content, i.e. no data
-# 5) format the query properly
+# [X] 1) set up logger + output
+# [X] 2) command line arguments for custom time interval
+# [X] 3) external file(s) for credentials
+# [ ] 4) error handling for requested content, i.e. no data
+# [ ] 5) format the query properly
+
+# argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--start", type=str,
+                    help="Specify the starting date for data collection in"
+                         " MMDDYYY format.")
+parser.add_argument("--end", type=str,
+                    help="Specify the ending date for data collection in"
+                         " MMDDYYY format.")
+parser.add_argument("-c", "--config", type=str,
+                    help="Specify the path to configuration file, containing"
+                         " a tokenstring and the MySQL server parameters.")
+parser.add_argument("-s", "--silent", action="store_true",
+                    help="No progress output to console, only critical "
+                    "errors will be printed.")
+parser.add_argument("-l", "--log", type=str, help="Specify a logfile.")
+args = parser.parse_args()
+
+# loading the config -------------------------------------------------------
+config = "config.json" if args.config is None else args.config
+
+with open(config, "r") as conf:
+    config = json.load(conf)
+
+# logger setup -------------------------------------------------------------
+# create logger instance
+logger = logging.getLogger("engagingNetworks")
+logger.setLevel(logging.DEBUG)  # general logging level
+
+if args.silent:
+    logger.setLevel(logging.CRITICAL)
+
+ch = logging.StreamHandler()  # channel -> console
+ch.setLevel(logging.INFO)
+
+# create formatter
+formatter = logging.Formatter("%(asctime)s\t%(name)s\t%(levelname)s\t"
+                              "%(message)s")
+
+ch.setFormatter(formatter)
+
+# add handlers to logger
+logger.addHandler(ch)
+
+if args.log:  # if an additional file logger is specified
+    fh = logging.FileHandler(args.log)
+    fh.setLevel(logging.INFO)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
